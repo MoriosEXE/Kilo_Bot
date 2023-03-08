77ec84ec695f2a8767008fc78c93028618698e32
Corona_chan <romain.lesoin14@gmail.com>
2023-03-08 15:59:19 +0100

    fin de journée
    très très peu productive trop d'interruption

--- Analyseur_V2/Assets/script/starter.cs
+++ Analyseur_V2/Assets/script/starter.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 public class starter : MonoBehaviour
 {
+    public GameObject kilo_bot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        string filePath = EditorUtility.OpenFilePanel("Select File", "", "txt");
+        if (filePath.Length != 0)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string content = reader.ReadToEnd();
+                Debug.Log(content);
+            }
+        }
     }
 
     // Update is called once per frame
