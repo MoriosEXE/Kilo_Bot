8df4388482e14197d94e4bcdb6a76a3d044f8308
Corona_chan <romain.lesoin14@gmail.com>
2023-03-20 17:13:53 +0100

    ajout rotation + interface analyseur

--- Analyseur_V2/Assets/script/starter.cs
+++ Analyseur_V2/Assets/script/starter.cs
@@ -6,6 +6,8 @@ using System.IO;
 using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 using UnityEngine.Windows;
 
 public class starter : MonoBehaviour
@@ -13,12 +15,18 @@ public class starter : MonoBehaviour
     public GameObject kilo_bot;
     public List<List<List<object>>> data;
     public List<Dictionary<int,GameObject>> instance_kilobot;
+    public InputField step_filed;
+    public InputField id_field;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        step_filed = GetComponentInChildren<InputField>();
     }
+
+
+
+
     public void generate_log()
     {
         foreach(var step in data)
@@ -26,7 +34,10 @@ public class starter : MonoBehaviour
             foreach(var bot in step)
             {
                 //Dictionary<int, GameObject> dict = new Dictionary<int, GameObject>();
-                Instantiate(kilo_bot, new Vector3((float)bot[1]*10, 0, (float)bot[2]*10), Quaternion.identity);
+                
+                Vector3 position = new Vector3((float)bot[1] * 10, 0, (float)bot[2] * 10); // Position d'instanciation
+                Quaternion rotation = Quaternion.Euler(0f,(float) bot[3], 0f); // Créer une rotation à partir de l'angle Y
+                Instantiate(kilo_bot, position, rotation); // Instancier l'objet avec la position et la rotation
                 //dict.Add((int)bot[0],Instantiate(kilo_bot,new Vector3((float) bot[1], (float)bot[2],0),Quaternion.identity));
                 //instance_kilobot.Add(dict);
             }
@@ -74,8 +85,9 @@ public class starter : MonoBehaviour
                             int idBot = int.Parse(bot.Split("=")[1].Split(" ")[1]);
                             float posX = float.Parse(bot.Split("=")[2].Split(" ")[1], CultureInfo.InvariantCulture.NumberFormat);
                             float posY = float.Parse(bot.Split("=")[3].Split(" ")[1], CultureInfo.InvariantCulture.NumberFormat);
-                            int opinion = int.Parse( bot.Split("=")[4].Split(" ")[1]);
-                            stepDt.Add(new List<object> { idBot, posX, posY ,opinion});
+                            float angle = float.Parse(bot.Split("=")[4].Split(" ")[1], CultureInfo.InvariantCulture.NumberFormat);
+                            int opinion = int.Parse( bot.Split("=")[5].Split(" ")[1]);
+                            stepDt.Add(new List<object> { idBot, posX, posY ,angle,opinion});
                         }
                     }
 
