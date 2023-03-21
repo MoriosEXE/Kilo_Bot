8ed048f38dfec700ef12d7b300e155ffc21a10db
Corona_chan <romain.lesoin14@gmail.com>
2023-03-21 16:18:36 +0100

    amélioration interface

--- Analyseur_V2/Assets/script/starter.cs
+++ Analyseur_V2/Assets/script/starter.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
@@ -14,16 +15,37 @@ public class starter : MonoBehaviour
 {
     public GameObject kilo_bot;
     public List<List<List<object>>> data;
-    public List<Dictionary<int,GameObject>> instance_kilobot;
-    public InputField step_filed;
-    public InputField id_field;
+    public List<Dictionary<int,GameObject>> instance_kilobot = new List<Dictionary<int,GameObject>>();
+    Dictionary<int, GameObject> dict_bot = new Dictionary<int, GameObject>();
+
+    private List<int> step_list = new List<int>();
+    private List<int> id_list = new List<int>();
 
     // Start is called before the first frame update
     void Start()
     {
-        step_filed = GetComponentInChildren<InputField>();
+        
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    public void read_step(string s)
+    {
+        step_list.Clear();
+        step_list = ParseNumbers(s);
+        foreach (int i in step_list) { print(i); }
+    }
+    public void read_id(string s)
+    {
+        id_list.Clear();
+        id_list= ParseNumbers(s);
+    }
 
 
 
@@ -31,16 +53,20 @@ public class starter : MonoBehaviour
     {
         foreach(var step in data)
         {
-            foreach(var bot in step)
+
+            dict_bot.Clear();
+            foreach (var bot in step)
             {
-                //Dictionary<int, GameObject> dict = new Dictionary<int, GameObject>();
-                
+
                 Vector3 position = new Vector3((float)bot[1] * 10, 0, (float)bot[2] * 10); // Position d'instanciation
                 Quaternion rotation = Quaternion.Euler(0f,(float) bot[3], 0f); // Créer une rotation à partir de l'angle Y
-                Instantiate(kilo_bot, position, rotation); // Instancier l'objet avec la position et la rotation
-                //dict.Add((int)bot[0],Instantiate(kilo_bot,new Vector3((float) bot[1], (float)bot[2],0),Quaternion.identity));
-                //instance_kilobot.Add(dict);
+                GameObject temp_bot = Instantiate(kilo_bot, position, rotation); // Instancier l'objet avec la position et la rotation
+                dict_bot.Add((int)bot[0], temp_bot);
+
             }
+            
+            instance_kilobot.Add(dict_bot);
+            
         }
     } 
 
@@ -106,9 +132,35 @@ public class starter : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    static List<int> ParseNumbers(string input)
     {
-        
+        List<int> numbers = new List<int>();
+        foreach (string group in input.Split(','))
+        {
+            if (Regex.IsMatch(group, @"^\d+$"))
+            {
+                // Add single number
+                numbers.Add(int.Parse(group));
+            }
+            else if (Regex.IsMatch(group, @"^\d+-\d+$"))
+            {
+                // Add range of numbers
+                Match match = Regex.Match(group, @"(\d+)-(\d+)");
+                int start = int.Parse(match.Groups[1].Value);
+                int end = int.Parse(match.Groups[2].Value);
+                for (int i = start; i <= end; i++)
+                {
+                    numbers.Add(i);
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Invalid input format");
+            }
+        }
+        // Sort and remove duplicates
+        return numbers.OrderBy(n => n).Distinct().ToList();
     }
 }
